fe9f4418bb2a7f562828e69e81df22af850fb9bb
__BK0717 <72550527+CanisMinor-1037@users.noreply.github.com>
2022-06-18 20:07:58 +0800

    Delete udp_pingpong.py

--- udp_pingpong.py
+++
@@ -1,9 +0,0 @@
-from tabnanny import verbose
-from scapy.all import *
-
-ip = IP(src="192.168.31.141", dst="192.168.31.137")
-data = b'Ping Pong Game'
-udp = UDP(sport=9090, dport=9090)
-
-pkt = ip/udp/data
-send(pkt, verbose=0)
